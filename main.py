652bdaabcf8e855582258625bac338cb20315cd4
alex <alexsychev27@gmail.com>
2023-08-29 18:30:59 +0300

    finished the integration of network and main but it not work becouse tensorflow packege

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from Camera.camera import get_frame, frame_to_image
+from Neural_network.build_model import Model
+from send_mail import gmail_sender, login
 
 
 def main():
+    session = login()
     while True:
         frame = get_frame()
-        prediction = predict(frame)
+        prediction = Model.predict(frame)
         if prediction:
-            send_mail(frame)
+            image_to_send = frame_to_image(frame)
+            gmail_sender(session, image_to_send)
             break
 
